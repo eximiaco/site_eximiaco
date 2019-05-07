f6d92d9a5def3b6fdf6ea2efcb885faaa469716f
Gabriel Rempel <gabriel@aztecweb.net>
2019-05-07 07:56:40 -0300

    Resolve "Home - Seção nossas especialidades"

--- src/page-templates/home.php
+++ src/page-templates/home.php
@@ -58,36 +58,40 @@ get_header(); ?>
 		</div>
 
 		<div class="front-page--purpose container">
-			<div class="front-page--purpose-image wow fadeIn">
-				<?php $purpose_image = get_field( 'purpose_image' ); ?>
-				<img src="<?php echo esc_url( wp_get_attachment_image_url( $purpose_image['ID'], 'medium_large' ) ); ?>" alt="Meu trabalho">
-			</div>
 			<div class="front-page--purpose-content">
 				<div class="front-page--purpose-title wow fadeIn">
-				<?php echo wp_kses_post( get_field( 'purpose_title' ) ); ?>
+					<?php echo wp_kses_post( get_field( 'purpose_title' ) ); ?>
 				</div>
-				<div class="purpose wow fadeIn">
-				<?php $icon_class = get_field( 'purpose_icon_1' ); ?>
-					<div class="purpose--icon">
-						<i class="<?php echo esc_attr( $icon_class ); ?>"></i>
-					</div>
-					<div class="purpose--content">
-						<div class="purpose--title">
-						<?php echo wp_kses_post( get_field( 'purpose_title_1' ) ); ?>
-						</div>
-					<?php echo wp_kses_post( get_field( 'purpose_text_1' ) ); ?>
-					</div>
+
+				<div class="purpose-image wow fadeIn">
+					<?php $purpose_image = get_field( 'purpose_image' ); ?>
+					<img src="<?php echo esc_url( wp_get_attachment_image_url( $purpose_image['ID'], 'medium_large' ) ); ?>" alt="Meu trabalho">
 				</div>
-				<div class="purpose wow fadeIn">
-				<?php $icon_class = get_field( 'purpose_icon_2' ); ?>
-					<div class="purpose--icon">
-						<i class="<?php echo esc_attr( $icon_class ); ?>"></i>
-					</div>
-					<div class="purpose--content">
-						<div class="purpose--title">
-						<?php echo wp_kses_post( get_field( 'purpose_title_2' ) ); ?>
+
+				<div class="purpose-info">
+					<div class="purpose wow fadeIn">
+					<?php $icon_class = get_field( 'purpose_icon_1' ); ?>
+						<div class="purpose--icon">
+							<i class="<?php echo esc_attr( $icon_class ); ?>"></i>
 						</div>
-					<?php echo wp_kses_post( get_field( 'purpose_text_2' ) ); ?>
+						<div class="purpose--content">
+							<div class="purpose--title">
+							<?php echo wp_kses_post( get_field( 'purpose_title_1' ) ); ?>
+							</div>
+						<?php echo wp_kses_post( get_field( 'purpose_text_1' ) ); ?>
+						</div>
+					</div>
+					<div class="purpose wow fadeIn">
+					<?php $icon_class = get_field( 'purpose_icon_2' ); ?>
+						<div class="purpose--icon">
+							<i class="<?php echo esc_attr( $icon_class ); ?>"></i>
+						</div>
+						<div class="purpose--content">
+							<div class="purpose--title">
+							<?php echo wp_kses_post( get_field( 'purpose_title_2' ) ); ?>
+							</div>
+						<?php echo wp_kses_post( get_field( 'purpose_text_2' ) ); ?>
+						</div>
 					</div>
 				</div>
 			</div>
