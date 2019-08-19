684f1987fdff395ff6432113e094ba1e5bc63700
Lucas Hart <lucas.hart@aztecweb.net>
2019-08-19 08:55:55 -0300

    Correções do jshint

--- app/assets/js/app/translate-request.js
+++ app/assets/js/app/translate-request.js
@@ -1,15 +1,16 @@
-function translate_request(element,language)
+function translate_request(element,language)// jshint ignore:line
 {
 
-    document.getElementById("request-message").innerHTML =  element.getAttribute('data-form-message');
-    document.getElementById("translate-request-input-name").setAttribute('placeholder', element.getAttribute('data-input-name'));
-    document.getElementById("translate-request-input-email").setAttribute('placeholder', element.getAttribute('data-input-email'));
-    document.getElementById("translate-request-submit").value = element.getAttribute('data-submit');
+    document.getElementById('request-message').innerHTML =  element.getAttribute('data-form-message');
+    document.getElementById('translate-request-input-name').setAttribute('placeholder', element.getAttribute('data-input-name'));
+    document.getElementById('translate-request-input-email').setAttribute('placeholder', element.getAttribute('data-input-email'));
+    document.getElementById('translate-request-submit').value = element.getAttribute('data-submit');
 
-    document.getElementById("language-message-success").value = element.getAttribute('data-message-success');
-    document.getElementById("language-message-error").value = element.getAttribute('data-message-error');
+    document.getElementById('language-message-success').value = element.getAttribute('data-message-success');
+    document.getElementById('language-message-error').value = element.getAttribute('data-message-error');
 
-    document.getElementById("language-input").value = language;
-    document.getElementById("translate-request-box").classList.add('animated', 'fadeInUp')
-    document.getElementById("translate-request-box").style.display = 'block'
+    document.getElementById('language-input').value = language;
+    document.getElementById('translate-request-box').classList.add('animated', 'fadeInUp');
+    document.getElementById('translate-request-box').style.display = 'block';
+
 }
