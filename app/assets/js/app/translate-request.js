08a28d48170e16d87be40f4fec95699b4010f9c1
Lucas hart <lucas.hart@aztecweb.net>
2019-08-19 11:29:27 -0300

    Resolve "Criar links com email e formulário para solicitar tradução"

--- app/assets/js/app/translate-request.js
+++ app/assets/js/app/translate-request.js
@@ -1,16 +1,22 @@
-function translate_request(element,language)// jshint ignore:line
-{
+define(function () {
+    function translate_request() {
+        document.getElementById('request-message').innerHTML =  this.getAttribute('data-form-message');
+        document.getElementById('translate-request-input-name').setAttribute('placeholder', this.getAttribute('data-input-name'));
+        document.getElementById('translate-request-input-email').setAttribute('placeholder', this.getAttribute('data-input-email'));
+        document.getElementById('translate-request-submit').value = this.getAttribute('data-submit');
 
-    document.getElementById('request-message').innerHTML =  element.getAttribute('data-form-message');
-    document.getElementById('translate-request-input-name').setAttribute('placeholder', element.getAttribute('data-input-name'));
-    document.getElementById('translate-request-input-email').setAttribute('placeholder', element.getAttribute('data-input-email'));
-    document.getElementById('translate-request-submit').value = element.getAttribute('data-submit');
+        document.getElementById('language-message-success').value = this.getAttribute('data-message-success');
+        document.getElementById('language-message-error').value = this.getAttribute('data-message-error');
 
-    document.getElementById('language-message-success').value = element.getAttribute('data-message-success');
-    document.getElementById('language-message-error').value = element.getAttribute('data-message-error');
+        document.getElementById('language-input').value = this.getAttribute('data-language');
+        document.getElementById('translate-request-box').classList.add('animated', 'fadeInUp');
+        document.getElementById('translate-request-box').style.display = 'block';
+    }
 
-    document.getElementById('language-input').value = language;
-    document.getElementById('translate-request-box').classList.add('animated', 'fadeInUp');
-    document.getElementById('translate-request-box').style.display = 'block';
+    var elements = document.querySelectorAll('.request-translate');
 
-}
+    elements.forEach(function (item) {
+        item.addEventListener('click', translate_request);
+    });
+});
+
