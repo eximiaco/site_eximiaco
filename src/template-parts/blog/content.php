0da707e9b2655891e0bcd4b90d5c6f6478065d7d
Lucas Hart <lucas.hart@aztecweb.net>
2019-09-12 11:04:56 -0300

    Adicionando as Categorias e séries na listagem do bliki

--- src/template-parts/blog/content.php
+++ src/template-parts/blog/content.php
@@ -18,10 +18,8 @@
 				<header class="listing-post__header">
 					<div class="listing-post__header-meta">
 						<?php
-							if ( 'bliki' !== get_post_type() ){
-								get_template_part( 'template-parts/blog/content-parts/category' );
-								get_template_part( 'template-parts/blog/content-parts/serie' );
-							}
+							get_template_part( 'template-parts/blog/content-parts/category' );
+							get_template_part( 'template-parts/blog/content-parts/serie' );
 						?>
 					</div>
 					<?php
